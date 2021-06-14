b83b0a6d487732854108b6420982f181488fa4ad
lucas-gtd <lucasgoutaudier@gmail.com>
2021-06-14 11:06:27 +0200

    fin quete

---
+++ src/main/java/com/example/demo/controller/DoctorController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DoctorController {
+    @GetMapping("/doctor/1")
+    @ResponseBody
+    public String doctorOne() {
+        return "William Hartnell";
+    }
+    @GetMapping("/doctor/10")
+    @ResponseBody
+    public String doctorTen() {
+        return "David Tennant";
+    }
+    @GetMapping("/doctor/13")
+    @ResponseBody
+    public String doctorTreize() {
+        return "Jodie Whittaker";
+    }
+}
